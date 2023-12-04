22abc1286d59fc9b4fe2f5e0227128ae29ba2353
robertmwenja4 <robertmwenja4@gmail.com>
2023-12-04 16:57:06 +0300

    Description leaterhead

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -7,8 +7,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width,initial-scale=1.0,user-scalable=0,minimal-ui">
-    <meta name="description" content="Vuexy admin is super flexible, powerful, clean &amp; modern responsive bootstrap 4 admin template with unlimited possibilities.">
-    <meta name="keywords" content="admin template, Vuexy admin template, dashboard template, flat admin template, responsive admin template, web app">
+    <meta name="description" content="Fuel Management Solution">
+    <meta name="keywords" content="fuel, management, solution">
     <meta name="author" content="PIXINVENT">
     <title>ORYX</title>
     <link rel="apple-touch-icon" href="{{asset('core/app-assets/images/ico/apple-icon-120.png')}}">
